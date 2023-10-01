114d258227936c52a3314171e6700d5cd24d772a
Dinesh kumar <reachtodinesh@gmail.com>
2023-10-01 18:37:33 +0530

    lottie animation integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import { WarningTriangle } from "iconoir-react";
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/computer-window.json";
 
 const Page = () => {
   return (
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
       <WarningTriangle className="text-yellow-500 text-4xl" />
       <p className="text-2xl">Under construction!</p>
+      <div className="flex flex-col w-full justify-center items-center">
+        <Lottie
+          animationData={animationData}
+          className="flex justify-center items-center"
+          loop={false}
+        />
+      </div>
     </div>
   );
 };
